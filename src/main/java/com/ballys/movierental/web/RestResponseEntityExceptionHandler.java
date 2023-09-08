5cbf654636f2319a507ccf17a79bc160d230655d
Mark Ryan Guamos <ryan.guamos@gmail.com>
2023-09-08 13:11:04 +0800

    added exception handling

--- src/main/java/com/ballys/movierental/web/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/ballys/movierental/web/RestResponseEntityExceptionHandler.java
@@ -3,6 +3,7 @@ package com.ballys.movierental.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,17 +15,9 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = { MovieRentalException.class })
-    protected ResponseEntity<Object> handleMovieRentalException(MovieRentalException ex, WebRequest request) {
+    public ResponseEntity<Object> handleMovieRentalException(MovieRentalException ex, WebRequest request) {
         log.error("MovieRentalException", ex);
-        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.UNPROCESSABLE_ENTITY, request);
+        return handleExceptionInternal(ex, ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.getMessage()), new HttpHeaders(), HttpStatus.UNPROCESSABLE_ENTITY, request);
     }
-
-    @ExceptionHandler(value = { Exception.class })
-    protected ResponseEntity<Object> handleMovieRentalException(Exception ex, WebRequest request) {
-        log.error("Exception", ex);
-        return handleExceptionInternal(ex, null, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
-    }
-
-
 
 }
